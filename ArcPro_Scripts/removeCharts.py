ab62493e9267b3167bef0573d89cf1854e4783d3
persephone.wong <persephone.w315@gmail.com>
2025-01-09 09:46:49 -0800

    fix syntax for removeCharts

--- ArcPro_Scripts/removeCharts.py
+++ ArcPro_Scripts/removeCharts.py
@@ -1,12 +1,12 @@
 """Import arcsys module."""
 import arcpy
 
-CurrentProject = arcpy.mp.ArcGISProject('current')
+CurrentProject = arcpy.mp.ArcGISProject('CURRENT')
 Maps = CurrentProject.listMaps("Data Themes")[0]
 Layers = Maps.listlayers()
 for layer in Layers:
     if layer.supports("CHARTS"):
-        Layers.removeLayer()
+        Layers.removeLayer(layer)
 
 ##Refreshes Active view    
 CurrentProject.activeView.camera.scale = CurrentProject.camera.scale + 0.05
