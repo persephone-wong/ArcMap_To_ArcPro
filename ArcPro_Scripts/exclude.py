c785d9121502b05f9e8b22d4d0eba0ab39fedd03
persephone.wong <persephone.w315@gmail.com>
2025-01-09 09:46:00 -0800

    add stages for exclude tool

--- ArcPro_Scripts/exclude.py
+++ ArcPro_Scripts/exclude.py
@@ -11,4 +11,20 @@ for layer in Layers:
         arcpy.AddMessage("##########################################")
     elif layer.isGroupLayer:
         if layer.isFeatureLayer:
-            if lyr.supports("WORKSPACEPATH"):
+            if layer.supports("WORKSPACEPATH"):
+                WorkspacePath = layer.workspacePath
+                DescribeWorkspacePath = arcpy.Describe(WorkspacePath)
+                WorkspacePathType = DescribeWorkspacePath.workspaceFactoryProgID
+            DescribeLayer = arcpy.Describe(layer)
+            SelectedFids = DescribeLayer.FIDSet
+            if len(SelectedFids) > 0:
+                QueryList = SelectedFids.replace(';', ',')
+                OidFieldName = arcpy.Describe(layer).OIDFieldName
+                if WorkspacePath == "esriDataSourcesGDB.FileGDBWorkspaceFactory.1":
+                    layer.definitionQuery = '{0} in ({1})'.format(arcpy.AddFieldDelimiters(layer, OidFieldName, QueryList))
+                elif WorkspacePath == "esriDataSourcesGDB.AccessWorkspaceFactory.1":
+                    layer.definitionQuery =  '[{0}] in ({1})'. format(arcpy.AddFieldDelimiters(layer, OidFieldName), QueryList).replace("\"","",2)
+                elif WorkspacePath == "esriDataSourcesGDB.SdeWorkspaceFactory.1": #Identifies SDE database (MWQM does not use them so this code might not work properly if actually applied)
+                    layer.definitionQuery =  '{0} in ({1})'. format(arcpy.AddFieldDelimiters(layer, OidFieldName), QueryList).replace("\"","",2)      
+                else: #Identifies other types of data file (Shapefiles)
+                    layer.definitionQuery =  '{0} in ({1})'. format(arcpy.AddFieldDelimiters(layer, OidFieldName), QueryList)
